7f1e4200c8c170b87091d2062e83b25934dc3fcb
Charles <charlesdpj78@gmail.com>
2025-03-24 14:53:18 +0100

    configuring db

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,9 +1,7 @@
 package config
 
 import (
-	"crypto/rand"
 	"log"
-	"math/big"
 	"os"
 	"strings"
 
@@ -33,7 +31,6 @@ func LoadConfig() (*Config, error) {
 		ApifyAPIKey:    os.Getenv("APIFY_API_KEY"),
 		Mode:           os.Getenv("MODE"),
 		Port:           os.Getenv("PORT"),
-		DBConnStr:      os.Getenv("POSTGRES_CONNECTION"),
 		APIKey:         os.Getenv("API_KEY"),
 		AllowedOrigins: parseAllowedOrigins(os.Getenv("ALLOWED_ORIGINS")),
 	}
@@ -42,18 +39,25 @@ func LoadConfig() (*Config, error) {
 		config.Port = "8080"
 	}
 
+	if config.Mode == "production" {
+		config.DBConnStr = os.Getenv("POSTGRES_CONNECTION_PROD")
+	} else {
+		config.DBConnStr = os.Getenv("POSTGRES_CONNECTION_LOCAL")
+	}
+
 	// Warn if secrets are missing
 	if config.APIKey == "" {
-		secret := "go9jajobs_api_key_" + randomString(32)
-		log.Fatal("⚠️  API_KEY not set. Generating a strong secret...", secret)
+		log.Fatal("API_KEY not set. Exiting.")
 
 	}
 
 	// Warn if secrets are missing
 	if config.Mode == "" {
-		log.Fatal("⚠️  MODE not set. Defaulting to 'dev'")
+		log.Println("MODE not set. Defaulting to 'dev'")
 		config.Mode = "dev"
 	}
+
+	log.Printf("Running in '%s' mode. Using database: %s", config.Mode, config.DBConnStr)
 
 	return config, nil
 }
@@ -63,19 +67,4 @@ func parseAllowedOrigins(origins string) []string {
 		return []string{"*"}
 	}
 	return strings.Split(origins, ",")
-}
-
-func randomString(length int) string {
-	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	b := make([]byte, length)
-
-	for i := range b {
-		n, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
-		if err != nil {
-			b[i] = 'a' // fallback character
-			continue
-		}
-		b[i] = charset[n.Int64()]
-	}
-	return string(b)
 }
